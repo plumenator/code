313328ed2471a806fa14131540e31b707bcbf795
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2010-04-23 02:06:35 +0530

    *** empty log message ***

--- opengl/tutorial/5-cube.py
+++ opengl/tutorial/5-cube.py
@@ -13,17 +13,14 @@ ERROR: PyOpenGL not installed properly.
         '''
   sys.exit()
 
-# Initial screen size
-screenWidth = 1000
-screenHeight = 1000
-
+angleInc = 5
 view_rotx, view_roty, view_rotz = (0, 0, 0)
 
 def display():
   
-  boxWidth = screenWidth/5
-  boxHeight = screenHeight/5
-  boxDepth = (screenHeight+screenWidth)/10
+  boxWidth = 1
+  boxHeight = 1
+  boxDepth = 1
   
    # clear all pixels 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -32,25 +29,28 @@ def display():
   glLoadIdentity()
 
   # Rotate the camera
-  glTranslate(screenWidth/2, screenHeight/2, 0)
+  z = -1
+  glTranslate(0, 0, (2*z-boxDepth)*0.5)
   glRotatef(view_rotx, 1, 0, 0)
   glRotatef(view_roty, 0, 1, 0)
-  glTranslate(-screenWidth/2, -screenHeight/2, 0)
-  
+  glTranslate(0, 0, -(2*z-boxDepth)*0.5)
+
    # Specify the vertices and indices
-  vertices = [0,        0,         0,
-              0,        boxHeight, 0,
-              boxWidth, boxHeight, 0,
-              boxWidth, 0,         0,
-              0,        0,         boxDepth,
-              0,        boxHeight, boxDepth,
-              boxWidth, boxHeight, boxDepth,
-              boxWidth, 0,         boxDepth]
+  vertices = [0,        0,         z,
+              0,        boxHeight, z,
+              boxWidth, boxHeight, z,
+              boxWidth, 0,         z,
+              0,        0,         z-boxDepth,
+              0,        boxHeight, z-boxDepth,
+              boxWidth, boxHeight, z-boxDepth,
+              boxWidth, 0,         z-boxDepth]
   
-  frontFace = [0, 1, 2, 3]
-  backFace  = [7, 6, 5, 4]
-  leftFace  = [3, 7, 4, 0]
-  rightFace = [1, 5, 6, 2]
+  frontFace = [3, 2, 1, 0]
+  backFace  = [4, 5, 6, 7]
+  leftFace  = [1, 5, 4, 0]
+  rightFace = [3, 7, 6, 2]
+  bottomFace = [0, 4, 7, 3]
+  topFace = [2, 6, 5, 1]
   
   glVertexPointer(3, GL_FLOAT, 0, vertices)
   
@@ -61,31 +61,33 @@ def display():
   glColor3f(1, 0, 0)
   
   # Move the box to the middle of the screen
-  glTranslate((screenWidth-boxWidth)/2, (screenHeight-boxHeight)/2, 0)
+  glTranslate(-boxWidth*0.5, -boxHeight*0.5, 0)
   
    # Draw the box
   glDrawElements(GL_LINE_LOOP, len(frontFace), GL_UNSIGNED_INT, frontFace)
   glDrawElements(GL_LINE_LOOP, len(backFace), GL_UNSIGNED_INT, backFace)
   glDrawElements(GL_LINE_LOOP, len(leftFace), GL_UNSIGNED_INT, leftFace)
   glDrawElements(GL_LINE_LOOP, len(rightFace), GL_UNSIGNED_INT, rightFace)
+  glDrawElements(GL_LINE_LOOP, len(bottomFace), GL_UNSIGNED_INT, bottomFace)
+  glDrawElements(GL_LINE_LOOP, len(topFace), GL_UNSIGNED_INT, topFace)
   
    # Disable vertex arrays
   glEnableClientState(GL_VERTEX_ARRAY)
   
    # Swap the buffers (glFLush() implicit)
-  glutSwapBuffers();
+  glutSwapBuffers()
   
 def special(k, x, y):
   global view_rotx, view_roty, view_rotz
   
   if k == GLUT_KEY_UP:
-    view_rotx += 1.0
+    view_rotx += 5
   elif k == GLUT_KEY_DOWN:
-    view_rotx -= 1.0
+    view_rotx -= 5
   elif k == GLUT_KEY_LEFT:
-    view_roty += 1.0
+    view_roty -= 5
   elif k == GLUT_KEY_RIGHT:
-    view_roty -= 1.0
+    view_roty += 5
   else:
         return
   glutPostRedisplay()
@@ -94,27 +96,22 @@ def reshape(width, height):
   # Setup the drawing area (bottomX bottomY, topX, topY)
   glViewport(0, 0, width, height)
 
-  # Update the screen width and height
-  global screenWidth, screenHeight
-  screenWidth = width
-  screenHeight = height
-  
   # Define the clipping volume (left, right, bottom, top, near, far)
   glMatrixMode(GL_PROJECTION)
   glLoadIdentity()
-  glOrtho(0, width, 0, height, -(screenHeight+screenWidth)/10, (screenHeight+screenWidth)/10);
+  glOrtho(-1, 1, -1, 1, -1, 200)
+  glFrustum(-1, 1, -1, 1, 1, 201)
+
   # Initialize the model-view matrix
   glMatrixMode(GL_MODELVIEW)
-  glLoadIdentity();
+  glLoadIdentity()
     
 def init():
    # select clearing color      
    glClearColor(0.0, 0.0, 0.0, 0.0)
-   glFrontFace(GL_CCW)
-   glCullFace(GL_BACK)
    glEnable(GL_CULL_FACE)
-   #glShadeModel(GL_SMOOTH)
-   #glEnable(GL_DEPTH_TEST)
+   glShadeModel(GL_SMOOTH)
+   glEnable(GL_DEPTH_TEST)
 
 #  Declare initial window size, position, and display mode
 #  (double buffer and RGBA).  Open window with "hello"
@@ -123,8 +120,8 @@ def init():
 #  Enter main loop and process events.
 
 glutInit(sys.argv)
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-glutInitWindowSize(screenWidth, screenHeight)
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+glutInitWindowSize(1000, 1000)
 glutInitWindowPosition(100, 100)
 glutCreateWindow("3D Box")
 init()
