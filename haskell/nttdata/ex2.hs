1577a768948aae8ec1fd724a5617bbb9de150ab5
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2012-04-24 01:15:10 +0530

    Solved ex2!

--- haskell/nttdata/ex2.hs
+++ haskell/nttdata/ex2.hs
@@ -3,10 +3,13 @@ import Control.Monad.State
 import System.Environment
 main = do
   cn <- getContents
-  (st, ad) <- runStateT(addLineNumber(lines cn)) 1
+  (st, ad) <- runStateT(addLineNumber(lines cn)) 0
   putStr $ unlines st
 addLineNumber :: [String] -> StateT Int IO [String]
 addLineNumber = f where
   f     = mapM g
-  g li = put 2 >> liftM(h li) get
+  g li = do
+    s <- get
+    put (s+1)
+    liftM(h li) get
   h li ln = show ln ++ " " ++ li
