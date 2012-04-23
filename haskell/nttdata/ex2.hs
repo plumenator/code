6225eb708065bd4d5bd5ea044e1155a44b4e47f7
Karthik Ravikanti <karthik@plumenator-fedora.(none)>
2012-04-23 22:29:26 +0530

    Added ex2.hs, given script. Solution's pending

---
+++ haskell/nttdata/ex2.hs
@@ -0,0 +1,12 @@
+module Main where
+import Control.Monad.State
+import System.Environment
+main = do
+  cn <- getContents
+  (st, ad) <- runStateT(addLineNumber(lines cn)) 1
+  putStr $ unlines st
+addLineNumber :: [String] -> StateT Int IO [String]
+addLineNumber = f where
+  f     = mapM g
+  g li = liftM(h li) get
+  h li ln = show ln ++ " " ++ li
