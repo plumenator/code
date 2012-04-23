ae37a7d6e936c63108a117df2e9fd6c5523263ba
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2012-04-24 03:08:43 +0530

    Solved ex3! Assumed that trailing chars are to be discarded for integers.

---
+++ haskell/nttdata/ex3.hs
@@ -0,0 +1,41 @@
+module Main where
+import Data.List(intercalate)
+import Data.List.Split(splitEvery)
+import System.Environment
+import Control.Monad.State
+
+main = do
+  ags <- getArgs
+  putStrLn $ similarScanf(ags !! 0)(ags !! 1)
+similarScanf :: String -> String -> String
+similarScanf = f where
+  f pt ags = intercalate "," $ evalState (parsePt (splitEvery 3 $ pt)) ags
+
+parsePt = mapM g where
+    g ('%':wd:kd)
+        | kd == "x" = liftM hex2int parsed
+        | kd == "d" = liftM str2int parsed
+        | otherwise = parsed
+        where parsed = (h (read (wd:[]) :: Int))
+    h width = do
+      s <- get
+      put (drop width s)
+      return (take width s)
+
+str2int :: String -> String
+str2int = takeWhile (\x -> x >= '0' && x <= '9')
+          
+hex2int :: String -> String
+hex2int str = show (hex2int' $ reverse str)
+    where
+      hex2int' :: String -> Int
+      hex2int' (x:xs) = (num x) + 16*(hex2int' xs)
+      hex2int' [] = 0
+      num x
+          | x == 'a' = 10
+          | x == 'b' = 11
+          | x == 'c' = 12
+          | x == 'd' = 13
+          | x == 'e' = 14
+          | x == 'f' = 15
+          | otherwise = (read (x:[]) :: Int)
